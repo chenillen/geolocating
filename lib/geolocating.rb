40ed6d54b234f56d6ec788916523fed25abf54ab
chenillen <chenillen@gmail.com>
2010-05-19 11:53:32 +0800

    generating modules

--- lib/geolocating.rb
+++ lib/geolocating.rb
@@ -1,1 +1,49 @@
-# Geolocating
+module Geolocating
+  module ClassMethods
+    
+  end
+  
+  module InstanceMethods
+    
+  end
+  
+  def self.included(receiver)
+    receiver.extend         ClassMethods
+    receiver.send :include, InstanceMethods
+  end
+  
+  module ActAsMapping
+    class GoogleGeoCoder
+      
+    end
+    
+    class IPGeoCoder
+      
+    end
+    
+    class AddrGeo < GoogleGeo
+      def initialize(args)
+        
+      end
+      
+      
+    end
+    
+    class LatlngGeo < GoogleGeo
+      def initialize(args)
+        
+      end
+      
+      
+    end
+    
+    class IPGeo < IPGeo
+      def initialize(args)
+        
+      end
+      
+      
+    end
+    
+  end
+end
